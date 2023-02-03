4cc2cbbaf6f87b4c800cd65181873221471d1610
Aagat Sharma <aagatsharma@pop-os.localdomain>
2023-02-03 13:33:38 +0545

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+// import { Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <CssBaseline />
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
